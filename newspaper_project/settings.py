c64944a5c88a6c69d922e140cf9eb595c3ec85e5
David Loreto <dloretosalas@gmail.com>
2022-10-09 16:36:46 -0400

    changing the database backend

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+#Third party
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -82,6 +85,9 @@ WSGI_APPLICATION = 'newspaper_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -89,6 +95,7 @@ DATABASES = {
     }
 }
 
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
